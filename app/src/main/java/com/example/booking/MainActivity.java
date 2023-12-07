92bb7b573104bbd45dac3336f2a640217e1bddac
Chhun Kongmeng <90774237+chhunkongmeng@users.noreply.github.com>
2023-12-07 19:13:54 +0700

    git remote add origin <https://github.com/ChhunKongmeng/Ticket.git> git push -u origin master

---
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.booking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.booking.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Replace with the name of your XML layout file
+    }
+}
